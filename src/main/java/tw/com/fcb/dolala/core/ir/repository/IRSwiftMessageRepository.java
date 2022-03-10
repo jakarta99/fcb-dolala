5c1681d0bef31fc4ba208813795d3d097c0f5b6a
jilldolala25 <jilldolala25@gmail.com>
2022-03-10 15:39:26 +0800

    commit

---
+++ src/main/java/tw/com/fcb/dolala/core/ir/repository/IRSwiftMessageRepository.java
@@ -0,0 +1,15 @@
+package tw.com.fcb.dolala.core.ir.repository;
+
+/**
+ * Copyright (C),2022-2022,FirstBank
+ * FileName: IRSwiftMessageRepository
+ * Author: Han-Ru
+ * Date: 2022/3/10 下午 03:34
+ * Description: IRSwiftMessageRepository
+ * Hisotry:
+ * <author>     <time>       <version>     <desc>
+ * 作者姓名       修改時間       版本編號       描述
+ */
+public interface IRSwiftMessageRepository {
+    void insert();
+}
---
+++ src/main/java/tw/com/fcb/dolala/core/ir/service/IRService.java
@@ -0,0 +1,20 @@
+package tw.com.fcb.dolala.core.ir.service;
+
+import tw.com.fcb.dolala.core.ir.web.cmd.SwiftMessageSaveCmd;
+
+/**
+ * Copyright (C),2022-2022,FirstBank
+ * FileName: IRService
+ * Author: Han-Ru
+ * Date: 2022/3/10 下午 03:32
+ * Description: 處理電文資料
+ * Hisotry:
+ * <author>     <time>       <version>     <desc>
+ * 作者姓名       修改時間       版本編號       描述
+ */
+public class IRService {
+
+    public void insert(SwiftMessageSaveCmd saveCmd){
+
+    }
+}
