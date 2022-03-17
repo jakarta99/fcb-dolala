9b6231da5ed2ff92a947a70397e90d88bda4b3f9
jilldolala25 <jilldolala25@gmail.com>
2022-03-17 16:51:02 +0800

    Response commit

---
+++ src/main/java/tw/com/fcb/dolala/core/common/enums/ResponseStatus.java
@@ -0,0 +1,16 @@
+package tw.com.fcb.dolala.core.common.enums;
+
+/**
+ * Copyright (C),2022-2022,FirstBank
+ * FileName: ResponseStatus
+ * Author: Han-Ru
+ * Date: 2022/3/17 下午 04:24
+ * Description: ResponseStatus
+ * Hisotry:
+ * <author>     <time>       <version>     <desc>
+ * 作者姓名       修改時間       版本編號       描述
+ */
+public enum ResponseStatus {
+    SUCCESS,
+    ERROR
+}
