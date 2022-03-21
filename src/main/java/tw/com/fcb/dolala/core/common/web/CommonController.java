f75ef5bbe77b765d625ea78f48a5db54c1aeee8f
jilldolala25 <jilldolala25@gmail.com>
2022-03-21 23:06:05 +0800

    CommonController 增加取號getFxno,geSeqNo,BranchCode API

--- src/main/java/tw/com/fcb/dolala/core/common/web/CommonController.java
+++ src/main/java/tw/com/fcb/dolala/core/common/web/CommonController.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -43,6 +44,8 @@ public class CommonController {
 	CustomerAccountService customerAccountService;
 	@Autowired
 	CustomerService customerService;
+	@Autowired
+	BranchCheckService branchCheckService;
 
 	// 匯率處理
 	@GetMapping("/fxrate")
@@ -98,7 +101,44 @@ public class CommonController {
 		log.info("呼叫讀取取號檔API查詢"+ systemType + "現已使用到第"+ serialNumber.getSerialNo()+ "號");
 		return serialNumber.getSerialNo();
 	}
-		
+	//取得外匯編號 FXNO
+	@PutMapping("/numberSerial")
+	@Operation(description = "取得外匯編號",summary = "取得外匯編號並更新取號檔")
+	public String getFxNo(String noCode,String systemType,String branch)  {
+		String fxNo = null;
+		try {
+			 fxNo =  serialNumberService.getFxNo(noCode,systemType,branch);
+
+			serialNumberService.updateSerialNumber(systemType,branch, Long.valueOf(fxNo.substring(5,10)));
+			log.info("呼叫取得外匯編號API,FXNO = "+fxNo+ ", 並更新取號檔成功"  );
+
+		} catch (Exception e) {
+			log.info("取得外匯編號錯誤" + e);
+
+		}
+		return  fxNo;
+	}
+	//取得IRCase seqNo
+	@PutMapping("/")
+	@Operation(description = "取得匯入IRCase SEQ_NO",summary = "取得匯入IRCase SEQ_NO並更新取號檔")
+	public String getSeqNo()  {
+		String irSeq = null;
+		final String branch = "999";
+		final String systemType = "IR_SEQ";
+		try {
+			irSeq =  serialNumberService.getIrSeqNo(systemType,branch);
+
+			serialNumberService.updateSerialNumber(systemType,branch, Long.valueOf(irSeq));
+			log.info("呼叫取得IRCase SEQ_NO API,SEQ_NO = "+irSeq+ ", 並更新取號檔成功"  );
+
+		} catch (Exception e) {
+			log.info("取得SEQ_NO錯誤" + e);
+
+		}
+		return  irSeq;
+	}
+
+
 	//顧客資料處理
 	@GetMapping("/customer")
 	@Operation(description = "以顧客帳號讀取顧客資料", summary = "讀取顧客資料")
@@ -117,6 +157,20 @@ public class CommonController {
 	
 	
 	//分行資料處理
+	@GetMapping("/branch")
+	@Operation(description = "傳入分行號碼取得分行字軌",summary = "以分行代號取得分行字軌")
+	public String getBranchCode(String branch){
+		String branchCode = null;
+		try {
+			branchCode = branchCheckService.getBranchCode(branch);
+			log.info("呼叫取得分行字軌API branch = " + branch + "字軌 = " + branchCode);
+		} catch (Exception e) {
+			log.info("讀取branchService錯誤" + e);
+
+		}
+		return branchCode;
+	}
+
 
 	//讀銀行檔
 	@GetMapping("/bank/{swiftcode}")
@@ -131,6 +185,8 @@ public class CommonController {
 		}
 
 		return bankDto;
-	}	
-	
+	}
+
+
+
 }
