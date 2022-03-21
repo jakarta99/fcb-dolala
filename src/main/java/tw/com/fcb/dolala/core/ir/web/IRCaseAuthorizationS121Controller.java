e8d6be518f66ea1d2254285d9b97e81ec9703864
ijoshua29 <83047238+ijoshua29@users.noreply.github.com>
2022-03-21 11:43:11 +0800

    update S121

--- src/main/java/tw/com/fcb/dolala/core/ir/web/IRCaseAuthorizationS121Controller.java
+++ src/main/java/tw/com/fcb/dolala/core/ir/web/IRCaseAuthorizationS121Controller.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.extern.slf4j.Slf4j;
+import tw.com.fcb.dolala.core.ir.repository.entity.IRCaseEntity;
 import tw.com.fcb.dolala.core.ir.service.IRCaseAuthorizationS121Service;
 import tw.com.fcb.dolala.core.ir.web.dto.IRCase;
 
@@ -22,14 +23,29 @@ public class IRCaseAuthorizationS121Controller {
 	@Autowired
 	IRCaseAuthorizationS121Service irCaseAuthorizationS121Service;
 	
+	
+	IRCaseEntity irCaseEntity;
 			
 	// ※※※ S121 API清單 ※※※
 	// S121I 查詢待放行資料
 	@PutMapping("/s121i")
     @Operation(description = "查詢待放行資料", summary="查詢待放行")
-    public IRCase qryWaitForAuthorization(String seqNo) {
-		IRCase irCase = irCaseAuthorizationS121Service.qryWaitForAuthorization(seqNo);		
-		return irCase;
+    public String qryWaitForAuthorization(String seqNo) {
+		String qryReturnMsg = "";
+		irCaseEntity = irCaseAuthorizationS121Service.qryWaitForAuthorization(seqNo);		
+		
+		if (irCaseEntity != null)
+		{
+			qryReturnMsg = "查詢待放行資料 = " + irCaseEntity.toString();
+			log.info(qryReturnMsg);			 
+		}
+		else
+		{
+			qryReturnMsg = "查無資料";
+			log.info(qryReturnMsg);
+		}
+		
+		return qryReturnMsg;
     }	
 	
 	// S121A 執行MT103放行
@@ -37,6 +53,7 @@ public class IRCaseAuthorizationS121Controller {
     @Operation(description = "執行MT103放行", summary="MT103放行")
     public String exeCaseAuthorization(String seqNo) {
 		String exeReturnMsg = irCaseAuthorizationS121Service.exeCaseAuthorization(seqNo);
+		log.info(exeReturnMsg);
 		return exeReturnMsg;
     }
 }
