a79c529f09e8f9d5a61efc792a61cc0e2f264e65
ijoshua29 <83047238+ijoshua29@users.noreply.github.com>
2022-03-28 14:52:45 +0800

    S131i2 update

--- src/main/java/tw/com/fcb/dolala/core/ir/service/IRAdvicePrintS131Service.java
+++ src/main/java/tw/com/fcb/dolala/core/ir/service/IRAdvicePrintS131Service.java
@@ -3,6 +3,7 @@ package tw.com.fcb.dolala.core.ir.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import tw.com.fcb.dolala.core.ir.repository.IRMasterRepository;
 import tw.com.fcb.dolala.core.ir.repository.entity.IRMaster;
+import tw.com.fcb.dolala.core.ir.web.dto.IRS131I2;
 
 @Slf4j
 @Transactional
@@ -18,13 +20,24 @@ public class IRAdvicePrintS131Service {
 
 	@Autowired
 	IRMasterRepository irMasterRepository;
-
+	
+	IRMaster irMaster;
+	IRS131I2 irS131I2;
 	
 	//S131R 「處理種類」為(0、1、2、7或8) 之發查電文。 ==>進行通知書列印(多筆)
 	public List<IRMaster> qryAdvicePrint(String branch)
 	{
 		List<IRMaster> listData = new ArrayList<IRMaster>();
 		listData = irMasterRepository.findByBeAdvBranchAndPrintAdvMk(branch, "N");
+		
+		for (int i = 0; i < listData.size(); i ++)
+		{
+			//update IRMaster PrintAdvMk = Y ：已列印通知書
+			irMaster = listData.get(i);
+			irMaster.setPrintAdvMk("Y");
+			irMasterRepository.save(irMaster);
+		}
+		
 		return listData;
 	}
 	
@@ -33,17 +46,37 @@ public class IRAdvicePrintS131Service {
 	{
 		int[] adviceCount = new int[2];
 		
+		//「受通知筆數」
 		List<IRMaster> listData = new ArrayList<IRMaster>();
 		listData = irMasterRepository.findByBeAdvBranch(branch);
 		
 		if (listData != null)
 			adviceCount[0] = listData.size();
 		
+		//「已印製通知書筆數」
 		listData = irMasterRepository.findByBeAdvBranchAndPrintAdvMk(branch, "Y");
 		
 		if (listData != null)
 			adviceCount[1] = listData.size();
 		
 		return adviceCount;
+	}
+	
+	// S131I2 "「處理種類」為(5或6) 之發查電文。==>回傳S1311畫面"
+	public List<IRS131I2> qryAdviceList(String branch)
+	{
+		List<IRMaster> listData = new ArrayList<IRMaster>();
+		List<IRS131I2> i2ListData = new ArrayList<IRS131I2>();
+		listData = irMasterRepository.findByBeAdvBranch(branch);
+		
+		for (int i = 0; i < listData.size(); i ++)
+		{			
+			irS131I2 = new IRS131I2();
+			irMaster = listData.get(i);
+			BeanUtils.copyProperties(irMaster, irS131I2);
+			i2ListData.add(irS131I2);
+		}
+		
+		return i2ListData;
 	}
 }
