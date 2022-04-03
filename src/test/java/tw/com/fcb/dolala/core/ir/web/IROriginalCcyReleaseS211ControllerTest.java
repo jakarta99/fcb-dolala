7262981f9c9eda4fdcf96642b7fabbedee3de576
sinjen626 <sinjen626@hotmail.com>
2022-04-03 23:14:04 +0800

    update & rename junit test

--- src/test/java/tw/com/fcb/dolala/core/ir/web/IROriginalCcyReleaseS211ControllerTest.java
+++ src/test/java/tw/com/fcb/dolala/core/ir/web/IROriginalCcyReleaseS211ControllerTest.java
@@ -6,19 +6,25 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import tw.com.fcb.dolala.core.common.http.Response;
+import tw.com.fcb.dolala.core.ir.web.dto.IRDto;
+
 /**
  * @author sinjen
  * S211-原幣匯入匯款解款
  */
 @SpringBootTest
-class IRDtoOriginalCcyReleaseS211ControllerTest {
+class IROriginalCcyReleaseS211ControllerTest {
 
 	@Autowired
 	IROriginalCcyReleaseS211Controller S211;
 	
+	// S211I 依匯入編號資料查詢解款資料 (A,C,D)
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testQryWaitForRelaseIRMaster() {
+		String irNo = "S1NHA00955";
+		Response<IRDto> response = S211.qryWaitForRelaseIRMaster(irNo);
+		assertEquals("0000", response.getCode());
 	}
 
 }
